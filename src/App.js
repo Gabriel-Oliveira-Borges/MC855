0e4f266423b686e77d875fa5b849c4722e487867
TIwashima <t187462@dac.unicamp.br>
2022-06-16 15:24:26 -0300

    Configura react-router e semanticUI

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import logo from "./logo.svg";
 import React from "react";
+import { Button } from 'semantic-ui-react'
 import "./App.css";
 
-class App extends React.Component {
-	render() {
-		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>
-            Edit <code>src/App.js</code> and save to reload.
-					</p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					> 
-            Learn React
-					</a>
-				</header>
-			</div>
-		);
-	}
+function App() {
+	return (
+		<div className="App">
+			<Button primary>Hello world!</Button>
+		</div>
+	)
 }
 
 export default App;
