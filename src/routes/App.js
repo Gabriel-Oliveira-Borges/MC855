25181e99a1377738e6d10e1b17999b313db108b1
TIwashima <t187462@dac.unicamp.br>
2022-06-17 11:34:58 -0300

    Cria componente TextButton

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,16 +1,21 @@
 import React from "react";
 import Option from '../components/Option'
+import TextButton from "../components/TextButton";
 import doctor from '../images/doctor 1.png'
 import './App.css'
 
 const App = () => {
 	return (
-		<div className="app">
+		<div className="app ui container">
 			<Option
 				className="link-option"
 				link="/opa"
 				image={doctor}
 				description="Já sou paciente do HC Unicamp"
+			/>
+			<TextButton
+				link="/especialidades"
+				text="VER ESPECIALIDADES DO AMBULATÓRIO de pediatria"
 			/>
 		</div>
 	)
