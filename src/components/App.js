2cbeb1cc03aa7df371a26ccf0bc7aed6067dddb0
TIwashima <t187462@dac.unicamp.br>
2022-06-16 22:51:19 -0300

    Cria option

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Option from './Option'
+import doctor from '../images/doctor 1.png'
+import './App.css'
+
+const App = () => {
+	return (
+		<div className="app">
+			<Option
+				className="link-option"
+				link="/opa"
+				image={doctor}
+				description="Já sou paciente do HC Unicamp"
+			/>
+		</div>
+	)
+}
+
+export default App;
