ab0a6a947b7c810d97edf1204c6d4f8d277875d0
TIwashima <t187462@dac.unicamp.br>
2022-07-15 09:38:04 -0300

    Edição de páginas

--- src/components/textEditor/TextEditor.js
+++ src/components/textEditor/TextEditor.js
@@ -1,13 +1,15 @@
 import React from "react"
 import ReactQuill from 'react-quill';
+import Translations from "../../constants/translations";
 import "./TextEditor.css";
 
 class TextEditor extends React.Component {
   render() {
     return (
         <div className="TextEditor">
+          {Translations[this.props.pageId]}
           <ReactQuill defaultValue={this.props.text}
-                      onChange={this.props.onChangeFn} />
+                      onChange={value => this.props.onChangeFn(this.props.pageId, value)} />
         </div>
     )
   }
